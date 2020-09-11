4cd14fba9c6bf7ef7b31b0d4b2f91ac9fee77cfd
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-09-11 12:23:00 -0400

    set up project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzOEfq9VyWUvarqn1o-9oCZMoAcbf9LXw",
+    authDomain: "challenge-823df.firebaseapp.com",
+    databaseURL: "https://challenge-823df.firebaseio.com",
+    projectId: "challenge-823df",
+    storageBucket: "challenge-823df.appspot.com",
+    messagingSenderId: "208534532221",
+    appId: "1:208534532221:web:2db08d5fc33969bf5cc114",
+    measurementId: "G-T14YJM0EMG"
+  };
