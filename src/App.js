59e6d04dbc50ce95b8e1a447b2397572faae389c
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-09-11 13:10:12 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
+    // BEM
     <div className="App">
-      <h1>Booya Amazon Clone!!! </h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
