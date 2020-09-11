34723a75152417125ab03a0188a2429d6b06e5ee
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-09-11 18:30:57 -0400

    checkout products (left side of chekcout page)

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+  // console.log("in chekcout js", basket);
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -12,8 +17,17 @@ function Checkout() {
           alt=""
         />
         <div>
-          <h2 className="checkout__title">Your Shopping Basket</h2>
-          
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 
